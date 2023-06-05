22a632575fe8d8e01e89d791eeb719175b04f3ec
IharTsykala <ihatsykala@kitrum.com>
2023-06-05 08:02:51 +0600

    main

--- product_service/bin/productService.ts
+++
@@ -1,8 +0,0 @@
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-
-import { ProductService } from "../lib/productService";
-
-const app = new cdk.App();
-new ProductService(app, "ProductServiceStack");
-app.synth();
