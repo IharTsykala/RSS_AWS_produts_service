47af35559563b934cbdf65c7507c332de919cd0e
IharTsykala <ihatsykala@kitrum.com>
2023-07-25 01:50:52 +0600

    feat: service with cash

--- bff-service/src/index.ts
+++ bff-service/src/index.ts
@@ -1,15 +1,42 @@
 import express from 'express'
 import axios from 'axios'
 import dotenv from 'dotenv'
+import NodeCache from 'node-cache'
+
 dotenv.config()
-
 const app = express()
-const port = 3000 // Change this port number as needed
+const port = process.env.PORT || 3000
 
-// Middleware to parse JSON bodies
+const cache = new NodeCache({ stdTTL: 120 })
+
 app.use(express.json())
 
-// BFF Service route
+app.get('/products', async (req, res) => {
+  const cachedProducts = cache.get('products')
+
+  if (cachedProducts) {
+    console.log('Took products from cash')
+    return res.json(cachedProducts)
+  }
+
+  try {
+    const recipientURL = process.env['products']
+    const response = await axios.get(`${recipientURL}/products`)
+    const products = response.data
+
+    cache.set('products', products, 120) // Cache for 2 minutes (120 seconds)
+    console.log('Products was cashed')
+
+    res.json(products)
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json(error.response.data)
+    } else {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+})
+
 app.all('/:recipientServiceName', async (req, res) => {
   try {
     const recipientServiceName = req.params.recipientServiceName
